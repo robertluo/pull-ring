e2c84f326255cdb4f6819abaeebe7c3b57f9c3d2
Luo Tian <lt@basecity.com>
2023-07-27 23:07:10 +0800

    define ring data schemas

--- src/robertluo/pull_ring.clj
+++ src/robertluo/pull_ring.clj
@@ -1,7 +1,37 @@
 (ns robertluo.pull-ring
   "Lasagna-pull extension for ring")
 
+;;----
+;; Ring data schemas
+;; see https://github.com/ring-clojure/ring/blob/master/SPEC
+
+(def ring-request-schema
+  "Data schema for Ring requests"
+  [:map
+   [:server-port :int]
+   [:server-name :string]
+   [:remote-addr :string]
+   [:uri [:and :string [:fn #(.startsWith % "/")]]]
+   [:query-string {:optional true} :string]
+   [:scheme [:enum :http :https]]
+   [:request-method :keyword]
+   [:protocol :string]
+   [:ssl-client-cert {:optional true} [:fn #(instance? java.security.cert.X509Certificate %)]]
+   [:headers [:map-of :string :string]]
+   [:body {:optional true} [:fn #(instance? java.io.InputStream %)]]])
+
 ^:rct/test
 (comment
-  (+ 1 2) ;=> 2
+  (require '[malli.core :as m])
+  (m/validate ring-request-schema {}) ;=> false
+  (m/validate ring-request-schema {:server-port 8080 :server-name "localhost" :remote-addr "localhost"
+                                   :uri "/hello" :scheme :https :request-method :get 
+                                   :protocol "HTTP/1.1" :headers {}}) ;=> true
   )
+
+(def ring-response-schema
+  "data schema for Ring responses"
+  [:map
+   [:status :int]
+   [:headers [:map-of :string :string]]
+   [:body {:optional true} [:fn #(instance? java.io.InputStream %)]]])
